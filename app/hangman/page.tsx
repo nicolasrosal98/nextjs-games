32481368504b2f12b3c4d77b1109563d49f5a248
Nicolas Rosal <nicolasrosal98@gmail.com>
2024-03-06 16:20:19 +0000

    Create initial states

--- app/hangman/page.tsx
+++ app/hangman/page.tsx
@@ -1,9 +1,31 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Hangman() {
+  const [wordToGuess, setWordToGuess] = useState<string>("unrented");
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([
+    "z",
+    "r",
+    "e",
+    "w",
+    "x",
+  ]);
+
   return (
     <div className="h-screen bg-gray-500 flex flex-col items-center justify-evenly">
       <h1 className="text-5xl text-center">Hangman</h1>
-      <h1 className="text-2xl text-center">Hangman</h1>
-      <h1 className="text-s text-center">Hangman</h1>
+      <h1 className="text-2xl text-center">{guessedLetters.join(", ")}</h1>
+      <h1 className="text-2xl text-center">{wordToGuess}</h1>
+      <button
+        onClick={() => {
+          setWordToGuess("Potato");
+          setGuessedLetters([]);
+        }}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+      >
+        reset
+      </button>
     </div>
   );
 }
