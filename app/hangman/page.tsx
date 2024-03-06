5e60b48b54d0e3d9fb460ccea03e6588ae92f526
Nicolas Rosal <nicolasrosal98@gmail.com>
2024-03-06 16:07:02 +0000

    Create hangman component

---
+++ app/hangman/page.tsx
@@ -0,0 +1,9 @@
+export default function Hangman() {
+  return (
+    <div className="h-screen bg-gray-500 flex flex-col items-center justify-evenly">
+      <h1 className="text-5xl text-center">Hangman</h1>
+      <h1 className="text-2xl text-center">Hangman</h1>
+      <h1 className="text-s text-center">Hangman</h1>
+    </div>
+  );
+}
