41d397b4219bdb4ff2788794d1ef220cdd27c15d
Nicolas Rosal <nicolasrosal98@gmail.com>
2024-03-07 15:30:26 +0000

    Create matchedLetters helper function

---
+++ app/hangman/helpers/matchedLetters.ts
@@ -0,0 +1,26 @@
+/**
+ * Adds together two numbers
+ * @param targetWord - the word to guess
+ * @param guessedArray - the users's letter guesses
+ * @returns an array with the matching letters and null if not matched
+ */
+
+function matchedLetters(targetWord: string, guessedArray: string[]): ((string | null)[] | number )[] {
+    let missCount = 0;
+    const targetLetters = targetWord.split("")
+    const outputArray: (string | null)[] = Array<(string | null)>(targetWord.length).fill(null);
+
+    for (const guessedLetter of guessedArray) {
+        if (targetLetters.indexOf(guessedLetter) === -1) missCount++ 
+
+        for (let index = 0; index < targetWord.length; index++) {
+            if (guessedLetter === targetWord[index]) {
+                outputArray[index] = guessedLetter
+            }
+        }
+    }
+
+    return [outputArray, missCount];
+}
+
+export { matchedLetters };
