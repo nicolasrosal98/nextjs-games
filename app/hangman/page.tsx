54d05548714188b840cdab74dd879cf3e3a6c791
Nicolas Rosal <nicolasrosal98@gmail.com>
2024-03-07 15:31:03 +0000

    Create keyboard for letter selection

--- app/hangman/page.tsx
+++ app/hangman/page.tsx
@@ -1,33 +1,48 @@
 "use client";
 
-import { useState } from "react";
+import { SyntheticEvent, useState } from "react";
 import { words } from "./words";
 import { sample } from "lodash";
 
 export default function Hangman() {
   const [wordToGuess, setWordToGuess] = useState<string>("unrented");
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([
-    "z",
-    "r",
-    "e",
-    "w",
-    "x",
-  ]);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
   const resetGame = () => {
-    const randomWord = sample(words);
-    if (!randomWord) {
+    const newRandomWord = sample(words);
+    if (!newRandomWord) {
       throw new Error("no words in words array");
     }
-    setWordToGuess(randomWord);
+    setWordToGuess(newRandomWord);
     setGuessedLetters([]);
   };
 
+  const handleLetterPick = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setGuessedLetters([...guessedLetters, e.currentTarget.innerHTML]);
+  };
+
+  const keyboard = alphabet.map((letter, key) => {
+    return (
+      <button
+        key={key}
+        onClick={handleLetterPick}
+        className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded m-1"
+      >
+        {letter}
+      </button>
+    );
+  });
+
   return (
-    <div className="h-screen bg-gray-500 flex flex-col items-center justify-evenly">
+    <div className="h-screen font-sans bg-blue-100 flex flex-col items-center justify-evenly">
       <h1 className="text-5xl text-center">Hangman</h1>
       <h1 className="text-2xl text-center">{guessedLetters.join(", ")}</h1>
       <h1 className="text-2xl text-center">{wordToGuess}</h1>
+      <div className="w-2/5 flex flex-wrap justify-center items-center">
+        {keyboard}
+      </div>
       <button
         onClick={resetGame}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
@@ -37,3 +52,5 @@ export default function Hangman() {
     </div>
   );
 }
+
+["#1b676b", "#519548", "#88c425", "#bef202", "#eafde6"];
