28768f7a2496fb91f072cf9ded8f219367764363
Nicolas Rosal <nicolasrosal98@gmail.com>
2024-03-14 11:28:40 +0000

    Reformat matchedLetters to handle null targetWords

--- app/hangman/helpers/matchedLetters.ts
+++ app/hangman/helpers/matchedLetters.ts
@@ -5,18 +5,23 @@
  * @returns an array with the matching letters and null if not matched
  */
 
-type Result = [(string | null)[], number]
+type LetterMatchResult = [(string | null)[], number]
 
-const expectedRes: Result = [['o', null, null, 'u'], 0]
+function matchedLetters(targetWord: string | null, guessedArray: string[]): LetterMatchResult {
+    
+    if (targetWord === null) {
+        return [[], 0]
+    }
 
-function matchedLetters(targetWord: string, guessedArray: string[]): Result {
     let missCount = 0;
     const targetLetters = targetWord.split("")
     const outputArray: (string | null)[] = Array<(string | null)>(targetWord.length).fill(null);
 
     for (const guessedLetter of guessedArray) {
-        if (targetLetters.indexOf(guessedLetter) === -1) missCount++ 
-
+        if (targetLetters.indexOf(guessedLetter) === -1) {
+            missCount++ 
+        }
+            
         for (let index = 0; index < targetWord.length; index++) {
             if (guessedLetter === targetWord[index]) {
                 outputArray[index] = guessedLetter
