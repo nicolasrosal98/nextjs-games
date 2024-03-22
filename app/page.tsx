0da9918e5911dd8b771cc604f5476555283f5bc9
Nicolas Rosal <nicolasrosal98@gmail.com>
2024-03-22 17:36:17 +0000

    Move font to top component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 import Header from "./components/header";
 import Profile from "./components/profile";
-import { Poppins } from "next/font/google";
-
-const poppins = Poppins({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <div className={poppins.className}>
+    <div>
       <Header />
       <Profile />
     </div>
